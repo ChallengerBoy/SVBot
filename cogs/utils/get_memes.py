c81cd4b46d3a3439940e3e4839f5942e680ff68e
Eshwar <reshwar2004@gmail.com>
2021-02-07 19:40:03 +0530

    changed to async praw

--- cogs/utils/get_memes.py
+++ cogs/utils/get_memes.py
@@ -3,21 +3,21 @@ import os
 import re
 from datetime import timedelta
 
-import praw
+import asyncpraw
 
 
-def get_meme():
-    agent = praw.Reddit(client_id='SkHY6dCDHkjQTQ',
+async def get_meme():
+    agent = asyncpraw.Reddit(client_id='SkHY6dCDHkjQTQ',
                         client_secret='xFNsUuhhfFqrSSrpfDOZRtpNKfLu4A',
                         user_agent='THE445GUY')
 
     while True:
         for setting in ('day', 'hour', 'week'):
             print(os.getcwd())
-            posts = agent.subreddit('dankmemes+memes').top(setting, limit=100)
+            posts = await agent.subreddit('dankmemes+memes').top(setting, limit=100)
             with open(f'{os.getcwd()}/cogs/json_files/posted.json', 'r') as f:
                 posted = json.load(f)
-            for post in posts:
+            async for post in posts:
                 if not post.stickied:
                     if post.title not in posted:
                         if (post.url.find('v.redd.it/') == -1) and not post.url.endswith('.gif'):
